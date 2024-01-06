7bc3efddac955572040ca4201da517ce68ccd82c
apromit pc <realapromit@gmail.com>
2024-01-06 23:57:04 +0300

    БД: родукты, Телеграм бот: исключение

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Brand::factory(20)->create();
-        Category::factory(20)->create();
+
+        Category::factory(10)
+            ->has(Product::factory(rand(5, 15)))
+            ->create();
     }
 }
